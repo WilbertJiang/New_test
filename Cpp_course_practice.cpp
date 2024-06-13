ef495fe8f0656b399fad8cdbcb3b6c835f2c2c76
LinJiang <jiangwilbert72@gmail.com>
2024-06-13 13:37:52 +0800

    test

---
+++ Cpp_course_practice.cpp
@@ -0,0 +1,12 @@
+﻿// Cpp_course_practice.cpp: 定义应用程序的入口点。
+//
+
+#include "Cpp_course_practice.h"
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello CMake!!!!!!!!!." << std::endl;
+	return 0;
+}
